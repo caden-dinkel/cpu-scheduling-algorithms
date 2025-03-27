e4b972215630e391a527838213705391524e6e14
Caden Dinkel <cadendinkel@Cadens-MacBook-Pro.local>
2025-03-26 19:15:15 -0500

    Changed tsconfig

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,8 @@ const nextConfig = {
   distDir: "out", // Default output directory
   trailingSlash: true, // Ensure proper file serving
   images: { unoptimized: true }, // Disable Next.js image optimization for static hosting
+  basePath: "/cpu-scheduling-algorithms",
+  assetPrefix: "/cpu-scheduling-algorithms/",
 };
 
 module.exports = nextConfig;
